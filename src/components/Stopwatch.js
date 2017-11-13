99d92ce4a07408c145a314aacc7858c428f4cd45
Ahkeem Lang <ahkeem@ip-192-168-5-106.ec2.internal>
2017-11-13 16:39:02 -0600

    Implementing life cycles and event handlers to the stopwatch file

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,1 +1,47 @@
 import React, { Component, PropTypes } from 'react';
+
+export default class Stopwatch extends Component {
+  state = {
+    running: false,
+    previouseTime: 0,
+    elapsedTime: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.onTick);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  onStart: function () {
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+    });
+  },
+
+  onStop: function () {
+    this.setState({
+      running: false,
+    });
+  },
+
+  onReset: function () {
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    });
+  },
+
+  onTick: function () {
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+        previousTime: Date.now(),
+      });
+    }
+  },
+}
